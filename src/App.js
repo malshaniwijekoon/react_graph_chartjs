cc008572034aee0f58fbc2a9e3e8f475b6915d10
malshaniwijekoon <malshani.wijekoon@gmail.com>
2023-02-07 20:54:50 +0530

    create react graph using chartjs library initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import CreateDoughnutData from './CreateDoughnutData';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateDoughnutData/>
+    </div>
+  );
+}
+
+export default App;
